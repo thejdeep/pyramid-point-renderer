7b1aac6746cd123d00e486e32c81aad9c13927ec
ricardo.marroquim <ricardo.marroquim@fedaa177-723b-0410-a87b-6305642fc211>
2008-06-24 21:32:02 +0000

    Creating subdirs...

--- pyramid_point_renderer/pyramid_point_render.cc
+++ pyramid_point_renderer/pyramid_point_render.cc
@@ -757,32 +757,32 @@ void PyramidPointRender::createShaders ( void ) {
   bool shader_inst_debug = 0;
 
   shader_projection = new glslKernel();
-  shader_projection->vertex_source("shader_point_projection.vert");
-  shader_projection->fragment_source("shader_point_projection.frag");
+  shader_projection->vertex_source("pyramid_point_renderer/shader_point_projection.vert");
+  shader_projection->fragment_source("pyramid_point_renderer/shader_point_projection.frag");
   shader_projection->install( shader_inst_debug );
 
   shader_analysis = new glslKernel();
-  shader_analysis->vertex_source("shader_analysis.vert");
-  shader_analysis->fragment_source("shader_analysis.frag");
+  shader_analysis->vertex_source("pyramid_point_renderer/shader_analysis.vert");
+  shader_analysis->fragment_source("pyramid_point_renderer/shader_analysis.frag");
   shader_analysis->install( shader_inst_debug );
 
   shader_copy = new glslKernel();
-  shader_copy->vertex_source("shader_copy.vert");  
-  shader_copy->fragment_source("shader_copy.frag");
+  shader_copy->vertex_source("pyramid_point_renderer/shader_copy.vert");  
+  shader_copy->fragment_source("pyramid_point_renderer/shader_copy.frag");
   shader_copy->install( shader_inst_debug );
 
   shader_synthesis = new glslKernel();
-  shader_synthesis->vertex_source("shader_synthesis.vert");
-  shader_synthesis->fragment_source("shader_synthesis.frag");
+  shader_synthesis->vertex_source("pyramid_point_renderer/shader_synthesis.vert");
+  shader_synthesis->fragment_source("pyramid_point_renderer/shader_synthesis.frag");
   shader_synthesis->install( shader_inst_debug );
 
   shader_phong = new glslKernel();
-  shader_phong->vertex_source("shader_phong.vert");
-  shader_phong->fragment_source("shader_phong.frag");
+  shader_phong->vertex_source("pyramid_point_renderer/shader_phong.vert");
+  shader_phong->fragment_source("pyramid_point_renderer/shader_phong.frag");
   shader_phong->install( shader_inst_debug );
 
   shader_show = new glslKernel();
-  shader_show->vertex_source("shader_show.vert");
-  shader_show->fragment_source("shader_show.frag");
+  shader_show->vertex_source("pyramid_point_renderer/shader_show.vert");
+  shader_show->fragment_source("pyramid_point_renderer/shader_show.frag");
   shader_show->install( shader_inst_debug );
 }
